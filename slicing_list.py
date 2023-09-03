42949abbf1fcc16ed888aace91865de3d4319da6
CORNELIUS <126483961+Cornelius-BobCat@users.noreply.github.com>
2023-09-03 20:47:45 +0200

    Create slicing_list.py

---
+++ slicing_list.py
@@ -0,0 +1,29 @@
+L = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+# indice de 2 a 6
+
+print(L[2:6])
+
+# toute la liste
+
+print(L[::])
+
+# de 5 en 5 depuis l'indice 4
+
+print(L[4::5])
+
+# de 3 en 3 depuis le 5iéme au 2ieme en partant de la fin
+
+print(L[5:-2:3])
+
+# de 5 en 5
+
+print(L[::5])
+
+# extraire de 5 en 5 en partant de la fin
+
+print(L[::-5])
+
+# indice pairs
+
+print(L[1::2])
